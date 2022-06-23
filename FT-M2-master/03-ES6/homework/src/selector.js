64334dc9880cf8b10151c224b74b936d5687ffb2
Paul C <pecardozo17@gmail.com>
2022-06-22 23:26:06 -0300

    Half done homework for the QuerySelector simulation

--- FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/03-ES6/homework/src/selector.js
@@ -18,7 +18,14 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if (selector[0]==='#') {
+    return "id";
+  }
+  if (selector[0]==='.') {
+    return "class";
+  }
+  if(selector.split('.').length === 2) return 'tag.class';
+  return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +37,27 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction=element=> '#'+element.id===selector;
   } else if (selectorType === "class") {
-    
+    matchFunction=element=> {
+      var clases = element.classList;
+      for (let i = 0; i < clases.length; i++){
+        if ('.' + clases[i] === selector){
+          return true
+        }
+      }
+      return false
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(element){
+      let [etiqueta, clase] = selector.split('.');
+      return matchFunctionMaker(etiqueta)(element) && matchFunctionMaker('.' + clase)(element) 
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(element){
+      return element.localName === selector
+      //return element.tagName.toLowerCase() === selector
+    }
   }
   return matchFunction;
 };
